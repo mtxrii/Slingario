5071a6140b1b2e4ccd7c040a681c0be961048d9d
Enrique <edavalos@ucsc.edu>
2020-08-07 00:34:07 -0700

    refactored blob.js

--- src/js/blob.js
+++ src/js/blob.js
@@ -1,11 +1,13 @@
-function Blob(x, y, r) {
-    this.pos = createVector(x, y);
-    this.r = r;
-    this.vel = createVector(0, 0);
-    this.anchored = false;
+class Blob {
+    constructor(x, y, r) {
+        this.pos = createVector(x, y);
+        this.r = r;
+        this.vel = createVector(0, 0);
+        this.anchored = false;
+    }
 
-    this.update = function() {
-        let newvel = createVector(mouseX-width/2, mouseY-height/2);
+    update() {
+        let newvel = createVector(mouseX - width / 2, mouseY - height / 2);
         if (!this.anchored) {
             newvel.setMag(3);
             this.vel.lerp(newvel, 0.1);
@@ -13,26 +15,27 @@ function Blob(x, y, r) {
         }
     }
 
-    this.show = function() {
+    show() {
         fill(255);
-        ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
+        ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
     }
 
-    this.eats = function(other) {
+    eats(other) {
         const d = p5.Vector.dist(this.pos, other.pos);
         if (d < this.r + other.r) {
             const sum = (Math.PI * this.r * this.r) + (Math.PI * other.r * other.r);
             this.r = Math.sqrt(sum / Math.PI);
             return true;
         }
-        else return false;
+        else
+            return false;
     }
 
-    this.anchor = function() {
+    anchor() {
         this.anchored = true;
     }
 
-    this.release = function() {
+    release() {
         this.anchored = false;
     }
 }
